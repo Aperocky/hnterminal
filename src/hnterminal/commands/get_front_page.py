85870f04928d319e4366211bc528c091f745fc27
Rocky Li <aperocky@gmail.com>
2023-03-07 19:59:05 -0500

    Make get_front_page responsive to terminal width, and use start-index and number instead of pages for more flexible control

--- src/hnterminal/commands/get_front_page.py
+++ src/hnterminal/commands/get_front_page.py
@@ -1,12 +1,16 @@
 import argparse
+import os
 from replbuilder import ReplCommand
 from urllib.parse import urlsplit
 from time import strftime, localtime
 from datetime import datetime
 
 
-HN_STORY_LINER = "\033[1;33m{rank: <7}\033[0m| {title: <80} | {score: <5} | {comment: <7} | {age: <10} | {base_url}"
-HN_STORY_TITLE = "\033[1;32m{rank: <7}| {title: <80} | {score: <5} | {comment: <7} | {age: <10} | {base_url}\033[0m"
+HN_LINER_SHORT_TOP = "\033[1;33m{rank: <7}| {title}\033[0m"
+HN_LINER_SHORT_BOTTOM = "{rank: <7}| \033[0;36m{author: <24}\033[0m | {score: <5} | {comment: <7} | {age: <10} | {base_url}"
+HN_LINER_SHORT_TITLE = "\033[1;32m{rank: <7}| {author: <24} | {score: <5} | {comment: <7} | {age: <10} | {base_url}\033[0m"
+HN_STORY_LINER = "\033[1;33m{rank: <7}\033[0m| {title: <80} | {score: <5} | {comment: <7} | {age: <10} | {author: <20} | {base_url}"
+HN_STORY_TITLE = "\033[1;32m{rank: <7}| {title: <80} | {score: <5} | {comment: <7} | {age: <10} | {author: <20} | {base_url}\033[0m"
 
 
 def get_age_str(timestamp):
@@ -23,34 +27,42 @@ def get_age_str(timestamp):
 def get_front_page_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--type', type=str, default="TOP", help="HN Story types, default to TOP, also try ASK, NEW, SHOW, JOB")
-    parser.add_argument('-p', '--page', type=int, default=0, help="Get the X page of HN stories")
+    parser.add_argument('-s', '--start-index', type=int, default=0, help="Start from index, if provided, overrides the page argument")
+    parser.add_argument('-n', '--number', type=int, default=30, help="Get these number of stories, default to 30")
     return parser
 
 
-def print_story_line(rank, item):
+def print_story_line(rank, item, width):
     age = get_age_str(item["time"])
     base_url = "NONE"
     if "url" in item:
         base_url = urlsplit(item["url"]).netloc
-    if item["type"] == "job":
-        print(HN_STORY_LINER.format(rank=rank, title=item["title"], score="N/A", comment="N/A", age=age, base_url=base_url))
-        return
-    print(HN_STORY_LINER.format(rank=rank, title=item["title"], score=item["score"], comment=item["descendants"], age=age, base_url=base_url))
+    score = item["score"] if "score" in item else "N/A"
+    comment = item["descendants"] if "descendants" in item else "N/A"
+    author = item["by"]
+    title = item["title"]
+    if width >= 155:
+        print(HN_STORY_LINER.format(rank=rank, title=item["title"], score=score, comment=comment, age=age, author=item["by"], base_url=base_url))
+    else:
+        print(HN_LINER_SHORT_TOP.format(rank=rank, title=title))
+        print(HN_LINER_SHORT_BOTTOM.format(rank="", author=author, score=score, comment=comment, age=age, base_url=base_url))
 
 
 def get_front_page(args, context):
     context.clear_pointers()
     context.get_story_list(args.type)
-    page = args.page
-    if page > 10:
-        raise ValueError("Not going that far back")
-    stories_of_interest = context.story_list[page*30:(page+1)*30]
-    print(HN_STORY_TITLE.format(rank="POINTER", title="TITLE", score="SCORE", comment="COMMENT", age="AGE", base_url="BASE URL"))
+    stories_of_interest = context.story_list[args.start_index:args.start_index+args.number]
+
+    width = os.get_terminal_size().columns
+    if width >= 155:
+        print(HN_STORY_TITLE.format(rank="POINTER", title="TITLE", score="SCORE", comment="COMMENT", age="AGE", author="AUTHOR", base_url="BASE URL"))
+    else:
+        print(HN_LINER_SHORT_TITLE.format(rank="POINTER", author="AUTHOR", score="SCORE", comment="COMMENT", age="AGE", base_url="BASE URL"))
     rank = 0
     for story_id in stories_of_interest:
         rank += 1
         context.store_item(story_id)
-        print_story_line(rank, context.loaded_items[story_id])
+        print_story_line(rank, context.loaded_items[story_id], width)
         context.store_pointer(rank, story_id)
 
 
